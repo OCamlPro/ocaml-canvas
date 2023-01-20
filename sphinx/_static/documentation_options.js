ab0f5e3e3ca5edc0b22faa30fce6748b9b2a9a2d
ddeclerck <30687695+ddeclerck@users.noreply.github.com>
2023-01-20 14:31:45 +0000

    Deploying to gh-pages from  @ 6bd7f6914a4fca95e6d0936614589348a757c8a2 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (b977657) (2022/12/12 11:19)',
+    VERSION: 'master (6bd7f69) (2023/01/20 14:31)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
