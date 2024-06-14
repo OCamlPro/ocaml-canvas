8f3ccd4f97abdf701c569e88cc51ed6b0d522ccf
Keryan-dev <keryan.didier@ocamlpro.com>
2024-06-14 10:09:03 +0000

    Deploying to gh-pages from  @ 4c4968dc44a41c4d6f5dc1b462b165f57b98c78d 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (23de080) (2024/06/14 08:18)',
+    VERSION: 'master (4c4968d) (2024/06/14 10:09)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
